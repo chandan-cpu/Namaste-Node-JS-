610b000deb5ea61e7a268f7366d20a271fbf9067
Chandan Boruah <chandanbo345@gmail.com>
2025-09-23 12:06:49 +0530

    Checking the routes

---
+++ server/readme.md
@@ -0,0 +1,1 @@
+// order of routes maters lot
--- server/server.js
+++ server/server.js
@@ -4,14 +4,23 @@ const app=express();
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-});
+// it only print the / data only after any routes
+// app.use('/',(req,res)=>{
+//     res.send('Hello World');
+// });
 
+// app.use('/test',(req,res)=>{
+//     res.send('This is a test route');
+// });
+// it only print the /test data only after any routes
 app.use('/test',(req,res)=>{
     res.send('This is a test route');
 });
 
+
+app.use('/',(req,res)=>{
+    res.send('Hello World');
+});
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
